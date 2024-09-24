99a0be12c688254b0beadc67c958fc7519b8a7a8
zhangyukang <3053161401@qq.com>
2024-09-24 14:32:35 +0800

    cookie.js设置时间

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -5,38 +5,38 @@ const refreshToken = 'REFRESH_TOKEN'
 const expiresIn = 'EXPIRES_IN'
 
 export function getAccessToken() {
-  return Cookies.get(accessToken)
+    return Cookies.get(accessToken)
 }
 
 export function setAccessToken(token) {
-  return Cookies.set(accessToken, token)
+    return Cookies.set(accessToken, token, {expires: 30})
 }
 
 export function removeAccessToken() {
-  return Cookies.remove(accessToken)
+    return Cookies.remove(accessToken)
 }
 
 export function getRefreshToken() {
-  return Cookies.get(refreshToken)
+    return Cookies.get(refreshToken)
 }
 
 export function setRefreshToken(token) {
-  return Cookies.set(refreshToken, token)
+    return Cookies.set(refreshToken, token, {expires: 30})
 }
 
 export function removeRefreshToken() {
-  return Cookies.remove(refreshToken)
+    return Cookies.remove(refreshToken)
 }
 
 export function setExpiresIn(seconds) {
-  return Cookies.set(expiresIn, seconds)
+    return Cookies.set(expiresIn, seconds, {expires: 30})
 }
 
 export function removeExpiresIn() {
-  return Cookies.remove(expiresIn)
+    return Cookies.remove(expiresIn)
 }
 
 export function getExpiresIn() {
-  return Cookies.get(expiresIn)
+    return Cookies.get(expiresIn)
 }
 
