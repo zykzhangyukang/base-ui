3ac03733d3dfc40e2ec9a38a1e596a0b94a688a0
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2024-09-25 20:42:22 +0800

    文件上传

--- src/components/UploadFile/index.vue
+++ src/components/UploadFile/index.vue
@@ -1,69 +1,74 @@
 <template>
-    <div>
-        <el-upload
-                ref="upload"
-                class="upload-demo"
-                action=""
-                :multiple="false"
-                :limit="1"
-                :file-list="fileList"
-                :show-file-list="false"
-                :auto-upload="true"
-                :http-request="httpRequest"
-        >
-            <el-button slot="trigger" type="primary">选择文件</el-button>
-        </el-upload>
-    </div>
+  <div>
+    <el-upload
+        ref="upload"
+        class="upload-demo"
+        action=""
+        :multiple="false"
+        :file-list="fileList"
+        :show-file-list="false"
+        :auto-upload="true"
+        :http-request="httpRequest"
+        :before-upload="beforeUpload"
+        :on-change="handleUploadChange"
+    >
+      <el-button slot="trigger" type="primary" :loading="loading" icon="el-icon-upload">
+        <span v-if="!loading">选择文件</span>
+        <span v-else>解析中...</span>
+      </el-button>
+    </el-upload>
+  </div>
 </template>
 
 <script>
-    import SparkMD5 from 'spark-md5';
-    import {cutFile} from "./cutFile";
-    import {uploadFileChunk} from "../../api/file";
+import SparkMD5 from 'spark-md5';
+import {cutFile} from "./cutFile";
+import {uploadFileChunk} from "@/api/file";
 
-    export default {
-        data() {
-            return {
-                worker: null,
-                fileList: []
-            };
-        },
-        methods: {
-            async httpRequest(option) {
-                const spark = new SparkMD5.ArrayBuffer();
-                const chunkList = await cutFile(option.file);
-                // 计算整个文件的 hash
-                chunkList.forEach(e => {
-                    spark.append(e.hash);
-                });
-                const hash = spark.end();
-                // 逐个上传分片
-                for (const chunk of chunkList) {
-                    await this._uploadChunk(hash, chunk);
-                }
-            },
-            async _uploadChunk(hash, chunk) {
-                console.log(chunk)
-                const formData = new FormData();
-                formData.append('hash', hash)
-                formData.append('file', chunk.blob)
-                formData.append('num', chunk.index)
-                try {
-                    const response = await uploadFileChunk(formData);
-                    console.log('Chunk uploaded successfully:', response);
-                } catch (error) {
-                    console.error('Error uploading chunk:', error);
-                    // 可以添加重试逻辑或其他处理逻辑
-                }
-            },
-        },
-        created() {
-        }
+export default {
+  data() {
+    return {
+      loading: false,
+      spark : new SparkMD5.ArrayBuffer(),
+      fileList: [],
     };
+  },
+  methods: {
+    handleUploadChange(file, fileList) {
+      if (fileList.length > 0) {
+        this.fileList = [fileList[fileList.length - 1]];
+      }
+    },
+    beforeUpload(file){
+    },
+    async httpRequest(option) {
+      this.loading = true;
+      const chunkList = await cutFile(option.file).finally(() => {
+        this.loading = false;
+      });
+      chunkList.forEach(e => {
+        this.spark.append(e.hash);
+      });
+      const hash = this.spark.end();
+      for (const chunk of chunkList) {
+         await this._uploadChunk(hash, chunk);
+      }
+    },
+    async _uploadChunk(hash, chunk) {
+      const formData = new FormData();
+      formData.append('hash', hash)
+      formData.append('file', chunk.blob)
+      formData.append('index', chunk.index)
+      return uploadFileChunk(formData);
+    },
+  },
+  created() {
+  }
+};
 </script>
 
 <style scoped>
-    .upload-demo {
-        margin: 20px;
-    }
+.upload-demo {
+  margin: 20px;
+}
 </style>
