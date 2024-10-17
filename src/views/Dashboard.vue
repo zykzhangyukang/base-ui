5abcb7b204a7a23a6db686ccb1c3619bafa3741c
zhangyukang <3053161401@qq.com>
2024-10-17 18:42:44 +0800

    代码优化+1

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-wrapper">
     <h3 style="text-align: center;margin-top: 100px;font-size: 21px;color: #303030;box-shadow:  #2f3e45">{{userInfo.realName}}你好，欢迎您使用系统！</h3>
-   <h6 style="text-align: center;margin-top: 50px;font-size: 11px;color: #f40;"> {{fundTips}}</h6>
+   <h6 style="text-align: center;margin-top: 30px;font-size: 11px;"> {{fundTips}}</h6>
   </div>
 </template>
 
