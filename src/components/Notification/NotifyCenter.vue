88ac245e56ecca0046d5f11d8d055760abb6acd0
zhangyukang <3053161401@qq.com>
2024-10-21 17:09:11 +0800

    代码优化+1

--- src/components/Notification/NotifyCenter.vue
+++ src/components/Notification/NotifyCenter.vue
@@ -6,66 +6,101 @@
             class="dialog-form"
             :before-close="handleClose"
     >
-      <el-radio-group v-model="module" size="mini" >
-        <el-radio-button  :label="notificationModuleGName[item.code]" :name="item.code" v-for="item in notificationModuleG" :key="item.code" ></el-radio-button>
-      </el-radio-group>
-      <el-table
-          :data="tableData"
-          stripe
-          style="width: 100%;margin-top: 10px">
-        <el-table-column
-            prop="date"
-            label="标题">
-        </el-table-column>
-        <el-table-column
-            prop="date"
-            label="发送人">
-        </el-table-column>
-        <el-table-column
-            prop="address"
-            label="创建时间">
-        </el-table-column>
-        <el-table-column
-            prop="address"
-            label="操作">
-        </el-table-column>
-      </el-table>
-      <el-pagination
-          small
-          background
-          style="margin-top: 10px;position: relative;"
-          layout="prev, pager, next"
-          :total="50">
-      </el-pagination>
+        <el-radio-group v-model="searchForm.module" size="mini" @input="handleInput">
+            <el-radio-button :label="item.code" :name="item.code" v-for="item in notificationModuleG" :key="item.code">
+                {{notificationModuleGName[item.code]}}
+            </el-radio-button>
+        </el-radio-group>
+        <el-table
+                v-loading="tableLoading"
+                :data="tableData"
+                stripe
+                style="width: 100%;margin-top: 10px">
+            <el-table-column
+                    prop="notificationType"
+                    width="100"
+                    label="消息标题">
+                <template slot-scope="scope">
+                    {{notificationTypeGName[scope.row.notificationType]}}
+                </template>
+            </el-table-column>
+            <el-table-column
+                    prop="data"
+                    label="内容">
+                <template slot-scope="scope">
+                    {{scope.row.data}}
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+                small
+                @current-change="handleCurrentChange"
+                background
+                style="margin-top: 10px;position: relative;"
+                layout="prev, pager, next"
+                :total="total">
+        </el-pagination>
     </el-dialog>
 </template>
 <script>
     import {adminDomain, formatConst, getConst} from "@/utils";
+    import {getNotificationPage} from "../../api/auth";
+
     export default {
         name: 'NotifyCenter',
         data() {
             return {
                 visible: false,
-                module: 'all',
-              tableData: []
+                tableLoading: false,
+                tableData: [],
+                total: 0,
+                searchForm: {
+                    currentPage: 1,
+                    pageSize: 5,
+                    module: 'all'
+                },
             }
         },
-        computed:{
-            notificationModuleG(){
+        computed: {
+            notificationModuleG() {
                 return getConst("notification_module", adminDomain)
             },
-            notificationModuleGName(){
+            notificationModuleGName() {
                 return formatConst(this.notificationModuleG);
             },
+            notificationTypeG() {
+                return getConst("notification_type", adminDomain)
+            },
+            notificationTypeGName() {
+                return formatConst(this.notificationTypeG);
+            },
         },
-        methods:{
+        methods: {
+            handleInput(val){
+                this.searchForm.currentPage = 1;
+                this.fetchData();
+            },
+            fetchData() {
+                this.tableLoading = true;
+                getNotificationPage(this.searchForm).then(res=>{
+                    this.tableData = res.result.dataList;
+                    this.total = res.result.totalRow;
+                }).finally(()=>{
+                    this.tableLoading = false;
+                })
+            },
+            handleCurrentChange(val) {
+                this.searchForm.currentPage = val;
+                this.fetchData();
+            },
             handleClose() {
                 this.visible = false;
             },
-            handleOpen(){
+            handleOpen() {
                 this.visible = true;
+                this.fetchData();
             },
-            onSubmit(){
+            onSubmit() {
                 this.handleClose();
             }
         },
@@ -74,5 +109,7 @@
     }
 </script>
 <style scoped lang="less">
-
+    .item {
+        margin-top: 5px;
+    }
 </style>
